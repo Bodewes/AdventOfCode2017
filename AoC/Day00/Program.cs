0ffdd79fce38d7ab0a2fdbe8aff31a0ea330080c
Martijn Bodewes <bodewes@gmail.com>
2017-11-29 21:55:05 +0100

    Initial commit, added Day 00 (template project) and basic UtilClass

---
+++ AoC/Day00/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace Day00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 01...");
+
+            // Read Input
+            var lines = Input.readFileToList("dummyInput.txt");
+
+            // Setup
+            var Solution = new Solution() { Data = lines };
+
+            // Solve
+            Solution.SolvePart1();
+
+            // Ready.
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution{
+
+        public List<string> Data { get; set; }
+
+        public void SolvePart1()
+        {
+            Data.ForEach(Console.WriteLine);
+        }
+
+    }
+
+}
---
+++ AoC/Util/Input.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Util
+{
+    static public class Input
+    {
+        static public List<string> readFileToList(string filePath)
+        {
+            return File.ReadAllLines(filePath).ToList();
+        }
+    }
+}
