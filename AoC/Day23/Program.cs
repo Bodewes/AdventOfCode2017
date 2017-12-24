2547a4ef10b98910afd59300da172aeb59c99336
Martijn Bodewes <bodewes@gmail.com>
2017-12-24 06:57:15 +0100

    Solved Day 23 and 24

---
+++ AoC/Day23/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 23...");
+
+            // Read Input
+            var lines = Util.Tools.readFileToList("input.txt");
+
+            // Setup
+            var Solution = new Solution() { Data = lines };
+
+            // Solve
+            Solution.SolvePart1(0, true);
+
+            Solution.Part2(108100, 125100);
+
+            //Solution.SolvePart1(a: 1);
+
+            // Ready.
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+
+        public void Part2(int from, int to)
+        {
+            int primes = 0;
+            int total = 0;
+            for(int i = from; i<=to; i = i + 17)
+            {
+                total++;
+                if (IsPrime(i))
+                {
+                    primes++;
+                }
+            }
+            Console.WriteLine($"{total}-{primes}={total - primes}");
+
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+
+        public List<string> Data { get; set; }
+
+        public long[] reg = new long[8];
+
+        public Instruction[] prog;
+
+        public void SolvePart1(long a = 0, bool debug = false)
+        {
+            prog = new Instruction[Data.Count()];
+            ParseInput();
+
+            reg[0] = a;
+
+            int mulCount = 0;
+            int opCount = 0;
+
+            long pc = 0;
+            while(pc>=0 && pc  < prog.Length)
+            {
+
+                if (opCount % 1_000 == 0 || debug)
+                {
+                    Console.WriteLine($"{opCount}: \t[{pc}] -> \t{string.Join(",\t", reg)}");
+                }
+
+                opCount++;
+
+                var i = prog[pc];
+                var x = i.XIsRef ? reg[i.X_ref] : i.X_const;
+                var y = i.YIsRef ? reg[i.Y_ref] : i.Y_const;
+                
+                switch (i.OpCode){
+                    case OpCode.set:
+                        reg[i.X_ref] = y;
+                        break;
+                    case OpCode.sub:
+                        reg[i.X_ref] = x - y;
+                        break;
+                    case OpCode.mul:
+                        mulCount++;
+                        reg[i.X_ref] = x * y;
+                        break;
+                    case OpCode.jnz:
+                        if (x != 0)
+                            pc += y-1;
+                        break;
+                }
+                pc++;
+
+
+            }
+
+            Console.WriteLine($"== MulCount: {mulCount}");
+
+
+        }
+
+        private void ParseInput()
+        {
+            int counter = 0;
+            Data.ForEach(line =>
+            {
+                var tokens = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var i = new Instruction();
+                OpCode opCode;
+                Enum.TryParse(tokens[0], out opCode);
+                i.OpCode = opCode;
+                var x = tokens[1];
+                if (long.TryParse(x, out long x_const))
+                {
+                    i.X_const = x_const;
+                    i.XIsRef = false;
+                }
+                else
+                {
+                    i.X_ref = x[0]-'a';
+                    i.XIsRef = true;
+                }
+
+                var y = tokens[2];
+                if (long.TryParse(y, out long y_const))
+                {
+                    i.Y_const = y_const;
+                    i.YIsRef = false;
+                }
+                else
+                {
+                    i.Y_ref = y[0]-'a';
+                    i.YIsRef = true;
+                }
+                prog[counter] = i;
+                counter++;
+            });
+        }
+    }
+
+    public enum OpCode
+    {
+        set,
+        sub,
+        mul,
+        jnz,
+
+    }
+
+    public class Instruction
+    {
+        public OpCode OpCode { get; set; }
+
+        public bool XIsRef { get; set; } // is X a reference or not
+        public bool YIsRef { get; set; }
+
+        public long X_const { get; set; }
+        public int X_ref { get; set; } // index in reg 'a' ==0, h='8'
+
+
+        public long Y_const { get; set; }
+        public int Y_ref { get; set; } // index in reg 'a' ==0, h='8'
+
+    }
+}
---
+++ AoC/Day24/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 24...");
+
+            // Read Input
+            var lines = Util.Tools.readFileToList("dummyInput.txt");
+
+            var part1Lines = Util.Tools.readFileToList("input.txt");
+
+            // Setup
+            var Solution = new Solution() { Data = lines };
+
+            // Solve
+            Solution.SolvePart1();
+
+            var Part1 = new Solution() { Data = part1Lines };
+            Part1.SolvePart1();
+
+            // Ready.
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+
+        public List<string> Data { get; set; }
+
+        public List<(int x, int y)> pieces = new List<(int x, int y)>();
+
+        private int max = 0;
+        private int longest = 0;
+        private int longest_max = 0;
+
+        public void SolvePart1()
+        {
+
+            Data.ForEach(line =>
+            {
+                var tokens = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                pieces.Add((Math.Min(int.Parse(tokens[0]), int.Parse(tokens[1])), Math.Max(int.Parse(tokens[0]), int.Parse(tokens[1]))));
+            });
+
+            var start = pieces.Where(p => p.x == 0 || p.y == 0);
+            foreach(var s in start)
+            {
+                var selectlist = pieces.ConvertAll(p => (p.x, p.y));
+                selectlist.Remove((s.x, s.y));
+                var newList = new List<(int x, int y)>();
+                if (s.x == 0)
+                    newList.Add((s.x, s.y));
+                else
+                    newList.Add((s.y, s.x));
+                Grow(newList, selectlist);
+            }
+
+            Console.WriteLine($"  == Max Bridge: {max} ==");
+            Console.WriteLine($"  == Longest Bridge:{longest} has strength {longest_max} ==");
+        }
+
+        public void Grow(List<(int x, int y)> list, List<(int x, int y)> availablePieces)
+        {
+            //printList(list);
+            // find piece
+            int end = list.Last().y;
+
+            var selection = availablePieces.Where(p => p.x == end || p.y == end);
+            if (selection.Count() == 0)
+            {
+                var value = list.Select(p => p.x + p.y).Sum();
+                //Console.WriteLine($" BRIDGE DONE -> {value}");
+                max = Math.Max(max, value);
+
+                // part2
+                if (list.Count() > longest)
+                {
+                    longest = list.Count();
+                    longest_max = value;
+                }else if (list.Count() == longest)
+                {
+                    longest_max = Math.Max(longest_max, value);
+                }
+                else
+                {
+                    // noop.
+                }
+
+            }
+            // append
+            foreach (var s in selection)
+            {
+                // copy to new lists
+                var newlist = list.ConvertAll(p => (p.x, p.y));
+                var selectlist = availablePieces.ConvertAll(p => (p.x, p.y));
+                selectlist.Remove((s.x, s.y));
+
+                if (s.x == end)
+                {
+                    newlist.Add((s.x, s.y));
+                }
+                else
+                {
+                    newlist.Add((s.y, s.x));
+                }
+                Grow( newlist, selectlist  );
+
+            }
+        }
+
+        public void printList( List<(int x, int y)> x)
+        {
+            Console.WriteLine(string.Join("--", x.Select(p => $"{p.x}/{p.y}")));
+        }
+
+    }
+
+}
